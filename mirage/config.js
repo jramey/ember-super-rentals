fdd8ee60440e3236925abd6704220cb5b47402c1
Jordan Ramey <rameyj05@gmail.com>
2018-10-25 20:33:08 -0400

    Building a Simple Component

--- mirage/config.js
+++ mirage/config.js
@@ -1,7 +1,7 @@
-export default function() {
+export default function () {
   this.namespace = '/api';
 
-  this.get('/rentals', function() {
+  this.get('/rentals', function () {
     return {
       data: [{
         type: 'rentals',
@@ -39,7 +39,7 @@ export default function() {
           image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
           description: "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
         }
-      }];
+      }]
     };
   });
 }
